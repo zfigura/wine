eb67a9eac5ab3dbd93f58618bc103ffce0d84fc9
Zebediah Figura <z.figura12@gmail.com>
2018-06-07 20:09:59 -0500

    server: Create server objects for eventfd-based synchronization objects.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5645,6 +5645,22 @@ struct terminate_job_reply
 };
 
 
+struct create_esync_request
+{
+    struct request_header __header;
+    unsigned int access;
+    int          initval;
+    int          flags;
+    /* VARARG(objattr,object_attributes); */
+};
+struct create_esync_reply
+{
+    struct reply_header __header;
+    obj_handle_t handle;
+    char __pad_12[4];
+};
+
+
 enum request
 {
     REQ_new_process,
@@ -5937,6 +5953,7 @@ enum request
     REQ_set_job_limits,
     REQ_set_job_completion_port,
     REQ_terminate_job,
+    REQ_create_esync,
     REQ_NB_REQUESTS
 };
 
@@ -6234,6 +6251,7 @@ union generic_request
     struct set_job_limits_request set_job_limits_request;
     struct set_job_completion_port_request set_job_completion_port_request;
     struct terminate_job_request terminate_job_request;
+    struct create_esync_request create_esync_request;
 };
 union generic_reply
 {
@@ -6529,6 +6547,7 @@ union generic_reply
     struct set_job_limits_reply set_job_limits_reply;
     struct set_job_completion_port_reply set_job_completion_port_reply;
     struct terminate_job_reply terminate_job_reply;
+    struct create_esync_reply create_esync_reply;
 };
 
 #define SERVER_PROTOCOL_VERSION 565
