fa6ae69fbfffcd602c7dad874e20a874932bc4d4
Zebediah Figura <z.figura12@gmail.com>
2018-06-10 16:11:54 -0500

    server, ntdll: Also store the esync type in the server.
    And validate it there while we're at it.
    
    We need to do this because CreateEvent() ignores the passed-in parameter
    when opening an existing event.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5651,13 +5651,15 @@ struct create_esync_request
     unsigned int access;
     int          initval;
     int          flags;
+    int          type;
     /* VARARG(objattr,object_attributes); */
+    char __pad_28[4];
 };
 struct create_esync_reply
 {
     struct reply_header __header;
     obj_handle_t handle;
-    char __pad_12[4];
+    int          type;
 };
 
 
@@ -6576,6 +6578,6 @@ union generic_reply
     struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 568
+#define SERVER_PROTOCOL_VERSION 569
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
