1cd152be8f78c4a76af8895a83d053b86750b3b3
Zebediah Figura <z.figura12@gmail.com>
2018-07-29 16:53:58 -0500

    ntdll, server: Specify EFD_SEMAPHORE on the server side.
    This just makes things cleaner; since we already pass the type to the server
    there's no reason to pass this as well.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5636,10 +5636,8 @@ struct create_esync_request
     struct request_header __header;
     unsigned int access;
     int          initval;
-    int          flags;
     int          type;
     /* VARARG(objattr,object_attributes); */
-    char __pad_28[4];
 };
 struct create_esync_reply
 {
@@ -6601,6 +6599,6 @@ union generic_reply
     struct get_esync_apc_fd_reply get_esync_apc_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 564
+#define SERVER_PROTOCOL_VERSION 565
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
