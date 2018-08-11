99601c12c0f916c39e1f38b8267fec7daf2d3c70
Zebediah Figura <z.figura12@gmail.com>
2018-08-11 18:00:03 -0500

    rpcrt4: Build the typelib proxy headers.

--- dlls/rpcrt4/ndr_typelib.c
+++ dlls/rpcrt4/ndr_typelib.c
@@ -20,6 +20,7 @@
 
 #define COBJMACROS
 #include "oaidl.h"
+#define USE_STUBLESS_PROXY
 #include "rpcproxy.h"
 #include "wine/debug.h"
 #include "wine/heap.h"
@@ -28,9 +29,62 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(ole);
 
+static HRESULT build_format_strings(ITypeInfo *typeinfo, WORD funcs,
+    const unsigned char **type_ret,
+    const unsigned char **proc_ret, unsigned short **offset_ret)
+{
+    return E_NOTIMPL;
+}
+
+/* Common helper for Create{Proxy,Stub}FromTypeInfo(). */
+static HRESULT get_iface_info(ITypeInfo *typeinfo, WORD *funcs, WORD *parentfuncs)
+{
+    TYPEATTR *typeattr;
+    ITypeLib *typelib;
+    TLIBATTR *libattr;
+    SYSKIND syskind;
+    HRESULT hr;
+
+    hr = ITypeInfo_GetContainingTypeLib(typeinfo, &typelib, NULL);
+    if (FAILED(hr))
+        return hr;
+
+    hr = ITypeLib_GetLibAttr(typelib, &libattr);
+    if (FAILED(hr))
+    {
+        ITypeLib_Release(typelib);
+        return hr;
+    }
+    syskind = libattr->syskind;
+    ITypeLib_ReleaseTLibAttr(typelib, libattr);
+    ITypeLib_Release(typelib);
+
+    hr = ITypeInfo_GetTypeAttr(typeinfo, &typeattr);
+    if (FAILED(hr))
+        return hr;
+    *funcs = typeattr->cFuncs;
+    *parentfuncs = typeattr->cbSizeVft / (syskind == SYS_WIN64 ? 8 : 4) - *funcs;
+    ITypeInfo_ReleaseTypeAttr(typeinfo, typeattr);
+
+    return S_OK;
+}
+
+static void init_stub_desc(MIDL_STUB_DESC *desc)
+{
+    desc->pfnAllocate = NdrOleAllocate;
+    desc->pfnFree = NdrOleFree;
+    desc->Version = 0x50002;
+    /* type format string is initialized with proc format string and offset table */
+}
+
 struct typelib_proxy
 {
     StdProxyImpl proxy;
+    IID iid;
+    MIDL_STUB_DESC stub_desc;
+    MIDL_STUBLESS_PROXY_INFO proxy_info;
+    CInterfaceProxyVtbl *proxy_vtbl;
+    unsigned short *offset_table;
 };
 
 static ULONG WINAPI typelib_proxy_Release(IRpcProxyBuffer *iface)
@@ -42,6 +96,12 @@ static ULONG WINAPI typelib_proxy_Release(IRpcProxyBuffer *iface)
 
     if (!refcount)
     {
+        if (This->proxy.pChannel)
+            IRpcProxyBuffer_Disconnect(&This->proxy.IRpcProxyBuffer_iface);
+        heap_free((void *)This->stub_desc.pFormatTypes);
+        heap_free((void *)This->proxy_info.ProcFormatString);
+        heap_free(This->offset_table);
+        heap_free(This->proxy_vtbl);
         heap_free(This);
     }
     return refcount;
@@ -57,27 +117,39 @@ static const IRpcProxyBufferVtbl typelib_proxy_vtbl =
 };
 
 static HRESULT typelib_proxy_init(struct typelib_proxy *This, IUnknown *outer,
-    IRpcProxyBuffer **proxy, void **obj)
+    ULONG count, IRpcProxyBuffer **proxy, void **obj)
 {
+    if (!fill_stubless_table((IUnknownVtbl *)This->proxy_vtbl->Vtbl, count))
+        return E_OUTOFMEMORY;
+
     if (!outer) outer = (IUnknown *)&This->proxy;
 
     This->proxy.IRpcProxyBuffer_iface.lpVtbl = &typelib_proxy_vtbl;
+    This->proxy.PVtbl = This->proxy_vtbl->Vtbl;
     This->proxy.RefCount = 1;
+    This->proxy.piid = This->proxy_vtbl->header.piid;
     This->proxy.pUnkOuter = outer;
 
     *proxy = &This->proxy.IRpcProxyBuffer_iface;
+    *obj = &This->proxy.PVtbl;
+    IUnknown_AddRef((IUnknown *)*obj);
 
-    return E_NOTIMPL;
+    return S_OK;
 }
 
 HRESULT WINAPI CreateProxyFromTypeInfo(ITypeInfo *typeinfo, IUnknown *outer,
     REFIID iid, IRpcProxyBuffer **proxy, void **obj)
 {
     struct typelib_proxy *This;
+    WORD funcs, parentfuncs, i;
     HRESULT hr;
 
     TRACE("typeinfo %p, outer %p, iid %s, proxy %p, obj %p.\n",
         typeinfo, outer, debugstr_guid(iid), proxy, obj);
+
+    hr = get_iface_info(typeinfo, &funcs, &parentfuncs);
+    if (FAILED(hr))
+        return hr;
 
     if (!(This = heap_alloc_zero(sizeof(*This))))
     {
@@ -85,9 +157,41 @@ HRESULT WINAPI CreateProxyFromTypeInfo(ITypeInfo *typeinfo, IUnknown *outer,
         return E_OUTOFMEMORY;
     }
 
-    hr = typelib_proxy_init(This, outer, proxy, obj);
-    if (FAILED(hr))
+    init_stub_desc(&This->stub_desc);
+    This->proxy_info.pStubDesc = &This->stub_desc;
+
+    This->proxy_vtbl = heap_alloc_zero(sizeof(This->proxy_vtbl->header) + (funcs + parentfuncs) * sizeof(void *));
+    if (!This->proxy_vtbl)
+    {
+        ERR("Failed to allocate proxy vtbl.\n");
         heap_free(This);
+        return E_OUTOFMEMORY;
+    }
+    This->proxy_vtbl->header.pStublessProxyInfo = &This->proxy_info;
+    This->iid = *iid;
+    This->proxy_vtbl->header.piid = &This->iid;
+    for (i = 0; i < funcs; i++)
+        This->proxy_vtbl->Vtbl[3 + i] = (void *)-1;
+
+    hr = build_format_strings(typeinfo, funcs, &This->stub_desc.pFormatTypes,
+        &This->proxy_info.ProcFormatString, &This->offset_table);
+    if (FAILED(hr))
+    {
+        heap_free(This->proxy_vtbl);
+        heap_free(This);
+        return hr;
+    }
+    This->proxy_info.FormatStringOffset = &This->offset_table[-3];
+
+    hr = typelib_proxy_init(This, outer, funcs + parentfuncs, proxy, obj);
+    if (FAILED(hr))
+    {
+        heap_free((void *)This->stub_desc.pFormatTypes);
+        heap_free((void *)This->proxy_info.ProcFormatString);
+        heap_free((void *)This->offset_table);
+        heap_free(This->proxy_vtbl);
+        heap_free(This);
+    }
 
     return hr;
 }
