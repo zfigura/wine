9b7bb124cdef59d1ba8d189c8e01070517d4d458
Zebediah Figura <z.figura12@gmail.com>
2018-08-13 21:35:06 -0500

    ntdll, server: Revert to old implementation of hung queue detection.
    By manually notifying the server every time we enter and exit a message wait.
    
    The hung queue logic keeps breaking. In the case of bug #9 it was breaking
    because we were waiting for more than 5 seconds on our queue and then someone
    sent us a message with SMTO_ABORTIFHUNG. Just stop fighting against the
    server and try to coöperate with it instead. It takes two extra server calls,
    but ideally the GUI thread isn't going to be in the same sort of performance-
    critical code that this patchset was written for.

--- server/trace.c
+++ server/trace.c
@@ -4577,6 +4577,11 @@ static void dump_get_esync_apc_fd_request( const struct get_esync_apc_fd_request
 {
 }
 
+static void dump_esync_msgwait_request( const struct esync_msgwait_request *req )
+{
+    fprintf( stderr, " in_msgwait=%d", req->in_msgwait );
+}
+
 static const dump_func req_dumpers[REQ_NB_REQUESTS] = {
     (dump_func)dump_new_process_request,
     (dump_func)dump_get_new_process_info_request,
@@ -4871,6 +4876,7 @@ static const dump_func req_dumpers[REQ_NB_REQUESTS] = {
     (dump_func)dump_open_esync_request,
     (dump_func)dump_get_esync_fd_request,
     (dump_func)dump_get_esync_apc_fd_request,
+    (dump_func)dump_esync_msgwait_request,
 };
 
 static const dump_func reply_dumpers[REQ_NB_REQUESTS] = {
@@ -5166,6 +5172,7 @@ static const dump_func reply_dumpers[REQ_NB_REQUESTS] = {
     (dump_func)dump_create_esync_reply,
     (dump_func)dump_open_esync_reply,
     (dump_func)dump_get_esync_fd_reply,
+    NULL,
     NULL,
 };
 
@@ -5463,6 +5470,7 @@ static const char * const req_names[REQ_NB_REQUESTS] = {
     "open_esync",
     "get_esync_fd",
     "get_esync_apc_fd",
+    "esync_msgwait",
 };
 
 static const struct
@@ -5530,6 +5538,7 @@ static const struct
     { "INVALID_LOCK_SEQUENCE",       STATUS_INVALID_LOCK_SEQUENCE },
     { "INVALID_OWNER",               STATUS_INVALID_OWNER },
     { "INVALID_PARAMETER",           STATUS_INVALID_PARAMETER },
+    { "INVALID_PARAMETER_4",         STATUS_INVALID_PARAMETER_4 },
     { "INVALID_READ_MODE",           STATUS_INVALID_READ_MODE },
     { "INVALID_SECURITY_DESCR",      STATUS_INVALID_SECURITY_DESCR },
     { "IO_TIMEOUT",                  STATUS_IO_TIMEOUT },
