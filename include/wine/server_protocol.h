1a490228fab2168251f90f5884ac98083d959299
Zebediah Figura <z.figura12@gmail.com>
2018-08-13 21:35:06 -0500

    ntdll, server: Revert to old implementation of hung queue detection.
    By manually notifying the server every time we enter and exit a message wait.
    
    The hung queue logic keeps breaking. In the case of bug #9 it was breaking
    because we were waiting for more than 5 seconds on our queue and then someone
    sent us a message with SMTO_ABORTIFHUNG. Just stop fighting against the
    server and try to coöperate with it instead. It takes two extra server calls,
    but ideally the GUI thread isn't going to be in the same sort of performance-
    critical code that this patchset was written for.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5720,6 +5720,17 @@ struct get_esync_apc_fd_reply
     struct reply_header __header;
 };
 
+
+struct esync_msgwait_request
+{
+    struct request_header __header;
+    int          in_msgwait;
+};
+struct esync_msgwait_reply
+{
+    struct reply_header __header;
+};
+
 enum esync_type
 {
     ESYNC_SEMAPHORE = 1,
@@ -6029,6 +6040,7 @@ enum request
     REQ_open_esync,
     REQ_get_esync_fd,
     REQ_get_esync_apc_fd,
+    REQ_esync_msgwait,
     REQ_NB_REQUESTS
 };
 
@@ -6331,6 +6343,7 @@ union generic_request
     struct open_esync_request open_esync_request;
     struct get_esync_fd_request get_esync_fd_request;
     struct get_esync_apc_fd_request get_esync_apc_fd_request;
+    struct esync_msgwait_request esync_msgwait_request;
 };
 union generic_reply
 {
@@ -6631,8 +6644,9 @@ union generic_reply
     struct open_esync_reply open_esync_reply;
     struct get_esync_fd_reply get_esync_fd_reply;
     struct get_esync_apc_fd_reply get_esync_apc_fd_reply;
+    struct esync_msgwait_reply esync_msgwait_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 576
+#define SERVER_PROTOCOL_VERSION 577
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
