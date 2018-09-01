7fc737f1e364f1952a00d17a05a70dd5be41d3dd
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 16:39:38 -0500

    ntdll: Implement NtResetEvent().

--- dlls/ntdll/sync.c
+++ dlls/ntdll/sync.c
@@ -438,6 +438,10 @@ NTSTATUS WINAPI NtSetEvent( HANDLE handle, LONG *prev_state )
 NTSTATUS WINAPI NtResetEvent( HANDLE handle, LONG *prev_state )
 {
     NTSTATUS ret;
+
+    if (do_fsync())
+        return fsync_reset_event( handle, prev_state );
+
     SERVER_START_REQ( event_op )
     {
         req->handle = wine_server_obj_handle( handle );
