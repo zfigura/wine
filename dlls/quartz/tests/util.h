163af66abd06949576f2c9199d7f480b18d43af1
Zebediah Figura <z.figura12@gmail.com>
2018-09-12 22:53:02 -0500

    quartz/tests/filtergraph: Add a test mp3 file.

--- dlls/quartz/tests/util.h
+++ dlls/quartz/tests/util.h
@@ -2,6 +2,7 @@
 
 static const WCHAR avifile[] = {'t','e','s','t','.','a','v','i',0};
 static const WCHAR mpegfile[] = {'t','e','s','t','.','m','p','g',0};
+static const WCHAR mp3file[] = {'t','e','s','t','.','m','p','3',0};
 
 WCHAR *load_resource(const WCHAR *name);
 
