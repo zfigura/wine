ce8486492119ff3b453cc9e82799099de1969b69
Zebediah Figura <z.figura12@gmail.com>
2018-09-15 23:17:32 -0500

    quartz/tests/avisplit: Add some tests for pin interfaces.

--- dlls/quartz/tests/avisplit.c
+++ dlls/quartz/tests/avisplit.c
@@ -88,7 +88,10 @@ static void check_interface_(unsigned int line, void *iface_ptr, REFIID iid, BOO
 
 static void test_interfaces(void)
 {
+    const WCHAR *filename = load_resource(avifile);
     IBaseFilter *filter = create_avi_splitter();
+    IFilterGraph2 *graph = connect_input(filter, filename);
+    IPin *pin;
 
     check_interface(filter, &IID_IBaseFilter, TRUE);
 
@@ -105,7 +108,28 @@ static void test_interfaces(void)
     check_interface(filter, &IID_IReferenceClock, FALSE);
     check_interface(filter, &IID_IVideoWindow, FALSE);
 
+    IBaseFilter_FindPin(filter, sink_name, &pin);
+
+    check_interface(pin, &IID_IPin, TRUE);
+
+    check_interface(pin, &IID_IMemInputPin, FALSE);
+todo_wine
+    check_interface(pin, &IID_IMediaSeeking, FALSE);
+
+    IPin_Release(pin);
+
+    IBaseFilter_FindPin(filter, source0_name, &pin);
+
+    check_interface(pin, &IID_IMediaSeeking, TRUE);
+    check_interface(pin, &IID_IPin, TRUE);
+
+    check_interface(pin, &IID_IAsyncReader, FALSE);
+
+    IPin_Release(pin);
+
     IBaseFilter_Release(filter);
+    IFilterGraph2_Release(graph);
+    DeleteFileW(filename);
 }
 
 static void test_enum_pins(void)
