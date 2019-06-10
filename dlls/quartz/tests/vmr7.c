a3f1457c6a383b178e3f5967073fd05d5ea376df
Zebediah Figura <z.figura12@gmail.com>
2019-06-09 21:37:19 -0500

    strmbase: Do not activate the window when changing its position.

--- dlls/quartz/tests/vmr7.c
+++ dlls/quartz/tests/vmr7.c
@@ -1842,8 +1842,8 @@ static void test_video_window_position(IVideoWindow *window, HWND hwnd, HWND our
     ok(rect.right == 400, "Got window right %d.\n", rect.right);
     ok(rect.bottom == 600, "Got window bottom %d.\n", rect.bottom);
 
-    todo_wine ok(GetActiveWindow() == our_hwnd, "Got active window %p.\n", GetActiveWindow());
-    todo_wine ok(GetTopWindow(NULL) == our_hwnd, "Got top window %p.\n", GetTopWindow(NULL));
+    ok(GetActiveWindow() == our_hwnd, "Got active window %p.\n", GetActiveWindow());
+    ok(GetTopWindow(NULL) == our_hwnd, "Got top window %p.\n", GetTopWindow(NULL));
 }
 
 static void test_video_window_owner(IVideoWindow *window, HWND hwnd, HWND our_hwnd)
