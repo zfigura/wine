e4126e19aeeaea8666cf1cf536b5d61def002046
Zebediah Figura <z.figura12@gmail.com>
2019-09-01 15:23:12 -0500

    strmiids: Define NONAMELESSUNION where necessary.

--- dlls/strmiids/evr.c
+++ dlls/strmiids/evr.c
@@ -16,6 +16,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#define NONAMELESSUNION
+
 /* Don't define those GUIDs here */
 #include "propidl.h"
 #include "mfidl.h"
