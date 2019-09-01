3183ae2b23e9aaa0014e1dd77ef6bfa3255a553f
Zebediah Figura <z.figura12@gmail.com>
2019-09-01 15:11:33 -0500

    wmcodecdspuuid: Define NONAMELESSUNION where necessary.

--- dlls/wmcodecdspuuid/wmcodecdspuuid.c
+++ dlls/wmcodecdspuuid/wmcodecdspuuid.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#define NONAMELESSUNION
+
 /* Don't define those GUIDs here */
 #include "strmif.h"
 #include "mediaobj.h"
